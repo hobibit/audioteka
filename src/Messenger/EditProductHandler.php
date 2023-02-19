9abf8b584fdb94217ad6f28dbb4a8a32f252b487
Maciej Fijałkowski <fijalkowskimaciej@gmail.com>
2023-02-19 19:58:55 +0100

    Option to edite products

---
+++ src/Messenger/EditProductHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Messenger;
+
+use App\Service\Catalog\ProductService;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class EditProductHandler implements MessageHandlerInterface
+{
+    public function __construct(private ProductService $service) { }
+
+    public function __invoke(EditProduct $command): void
+    {
+        $this->service->edit($command->productId, $command->name, $command->price);
+    }
+}
