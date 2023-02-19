7ddf671e923a12af25ed986ab668a0b83d55ec9c
Maciej Fijałkowski <fijalkowskimaciej@gmail.com>
2023-02-19 11:48:06 +0100

    Adding same product to cart multiple times

---
+++ src/Entity/CartProducts.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CartProductRepository::class)]
+class CartProducts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\ManyToOne(inversedBy: 'cartProducts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Cart $cart;
+
+    #[ORM\ManyToOne(inversedBy: 'cartProducts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Product $product;
+
+    public function __construct(Cart $cart, Product $product)
+    {
+        $this->cart = $cart;
+        $this->product = $product;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCart(): ?Cart
+    {
+        return $this->cart;
+    }
+
+    public function setCart(?Cart $cart): self
+    {
+        $this->cart = $cart;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+}
